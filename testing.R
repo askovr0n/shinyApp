70821e1138b210cd8489a61d63afa20e3b8c799b
Robert Kowalczyk <robertkowalczyk011@gmail.com>
2022-06-15 17:26:08 +0200

    Small changes

--- testing.R
+++ testing.R
@@ -8,8 +8,8 @@ library(stats)
 library(ggpubr)
 library(factoextra)
 
-Sys.setenv(SPOTIFY_CLIENT_ID = '65bd18c74fd34be0a24117ab05b98b40')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = '34bd47c92edf465993287ff4b707bf38')
+Sys.setenv(SPOTIFY_CLIENT_ID = 'xxx')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = 'xxxx')
 
 access_token <- get_spotify_access_token()
 
@@ -28,17 +28,17 @@ get_my_recently_played(limit = 5) %>%
 11142493166
 l72qiwa7koqcqmlxkdertq40q
 
-my_id <- '11142493166'
+my_id <- '1166825425'
 my_plists <- get_user_playlists(my_id)
 
 my_plists2 <- my_plists %>% 
-  filter(name %in% c('You & I Radio', 'Dance Party')) %>% 
+  filter(name %in% c('You & I Radio')) %>% 
   select(id)
 
 tracks <- my_plists2[['id']] %>% lapply(get_playlist_tracks)
-features <- tracks %>% select(track.id) %>% lapply(get_track_audio_features) %>% tibble()
+features <- tracks[[1]] %>% select(track.id) %>% lapply(get_track_audio_features) %>% tibble()
 
-tracks2 <- tracks %>%
+tracks2 <- tracks[[1]] %>%
   left_join(features[[1]][[1]], by = c("track.uri" = "uri"))
 
 tracks2 %>% 
@@ -48,3 +48,35 @@ tracks2 %>%
 tracks2 %>% 
   select(danceability:tempo) %>%
   fviz_cluster(kmeans2, .)
+
+
+all_my_fav_tracks <-
+  # This is somehow tough to read, but I lOVE PIPELINE!
+  # FIRST we send get_my_saved_tracks request, set include_meta_info to TRUE, will return the number of all tracks in total. After that, we request 50 items per time, therefore, the looptime should be 50 divide the length of tracks.
+  # Not all of us is lucky man, so if the number of tracks can't be divided exactly, we make one more time offset.
+  ceiling(get_my_saved_tracks(include_meta_info = TRUE)[['total']] / 50) %>%
+  # Generate a sequence by looptimes.
+  seq() %>%
+  # PlZ remember, the offset is start from zero, so we minus 1 from the offset. And everytime, we jump 50 items, because we have already request 50 items every time.
+  # Every loop, we get a data.frame with 50 rows, once the loop finished, we got a list with looptime piece data.frame, with reduce(rbind), we can merge all the data.frames as one.
+  map(function(x) {
+    get_my_saved_tracks(limit = 50, offset = (x - 1) * 50)
+  }) %>% reduce(rbind)
+
+
+artist_from_fav_tracks <-
+  all_my_fav_tracks %>%
+  select(track.artists) %>%
+  reduce(rbind) %>%
+  reduce(rbind) %>%
+  # I don't think we need Urls in further analyses, id (unique mark of artist) and name are selected here.
+  select(id, name)
+
+track_num_artist <-
+  artist_from_fav_tracks %>%
+  count(id, sort = TRUE) %>%
+  left_join(artist_from_fav_tracks, by = 'id',.) %>%
+  unique() %>%
+  select(-id) %>%
+  top_n(20, n) %>% 
+  arrange(desc(n))
